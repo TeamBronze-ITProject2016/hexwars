9ee6abea2f58c3b4f44f53ad0bf22b8e677b4201
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2016-08-25 21:48:07 +1000

    added basic rotation, rotates immediately in direction of mouse

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,7 +9,7 @@ using System.Collections;
 public class Player : Entity
 {
     public Rigidbody2D rb;
-    public float acceleration = 9.81f;
+    public float Acceleration;
 
     /*Initialise*/
     void Start()
@@ -24,8 +24,8 @@ public class Player : Entity
         {
             Debug.Log("Player/FixedUpdate(): Mouse Button Down");
             Vector2 coordinate = (Vector2)Input.mousePosition; /* CHANGE TO USE INPUT CONTROLLER CLASS */
-            RotateToPoint(coordinate);
             MoveForward(coordinate);
+            RotateToPoint(coordinate);
         }
     }
 
@@ -35,11 +35,25 @@ public class Player : Entity
          * Deceleration can probably be handled via rigidbody linear drag parameter */
         Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(coord.x, coord.y, 1));
 
-        rb.AddForce(p * acceleration);
+        Debug.Log("Player/MoveForward(): Adding force of size " + p);
+
+        rb.AddForce(p * Acceleration);
     }
 
-    void RotateToPoint(Vector2 x)
+    void RotateToPoint(Vector2 coord)
     {
         /* Rotate hexagon at a constant rate towards a certain point */
+
+        //find the vector pointing from our position to the target
+        Vector3 p = new Vector3(rb.position.x, rb.position.y, 1) - Camera.main.ScreenToWorldPoint(new Vector3(coord.x, coord.y, 1));
+
+        Debug.Log("Player/RotateToPoint(): Rotating to vector " + p);
+
+        //rotate us over time according to speed until we are in the required rotation
+        if (p != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(p.y, p.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
