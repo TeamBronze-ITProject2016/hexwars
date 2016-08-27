06494a5b0edf516ab95b0e1cf78a53cb7e22f1ae
jgrooby <jgrooby@student.unimelb.edu.au>
2016-08-27 17:45:22 +1000

    wrote simple camera follow script, added CameraFollow.cs to player prefab/object, renamed camera GameObject to 'Camera', made it so that GameObjects are not children of Camera, removed PartAdder.cs component from Camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace TeamBronze.HexWars
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        Transform cameraTransform;
+
+        // Use this for initialization
+        void Start()
+        {
+            /* TODO: Check if attached to local player */
+            if(true) {
+                cameraTransform = Camera.main.transform;
+            }
+        }
+
+        void LateUpdate()
+        {
+            /* Set camera position to this GameObject's position */
+            cameraTransform.position = new Vector3(transform.position.x, transform.position.y, cameraTransform.position.z);
+        }
+    }
+}
+
