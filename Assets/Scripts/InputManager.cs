6230cc8f2478d27613657ae933673d42a7835cef
jgrooby <jgrooby@student.unimelb.edu.au>
2016-08-27 18:34:04 +1000

    added InputManager.cs

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace TeamBronze.HexWars
+{
+    public class InputManager : MonoBehaviour
+    {
+        [Tooltip("0 = mouse, 1 = ...")]
+        public int inputType = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        bool IsActive()
+        {
+            /* Mouse */
+            if (inputType == 0)
+            {
+                return Input.GetMouseButton(0);
+            }
+            else
+            {
+                throw new System.Exception("Invalid inputType value");
+            }
+        }
+
+        Vector2 GetPos()
+        {
+            /* Mouse */
+            if (inputType == 0)
+            {
+                return (Vector2)Input.mousePosition;
+            }
+            else
+            {
+                throw new System.Exception("Invalid inputType value");
+            }
+        }
+    }
+}
