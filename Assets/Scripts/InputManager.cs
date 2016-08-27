6fced2edae32dfbb163d7647673de10538cd29f0
jgrooby <jgrooby@student.unimelb.edu.au>
2016-08-27 17:54:34 +1000

    added InputManager game object and empty InputManager.cs script

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace TeamBronze.HexWars
+{
+
+    public class InputManager : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
