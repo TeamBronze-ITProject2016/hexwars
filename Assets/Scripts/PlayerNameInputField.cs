d5ed224186b1e56032b279c0be43eae814aa86db
jgrooby <jgrooby@student.unimelb.edu.au>
2016-08-28 18:29:14 +1000

    some initial networking features set up

--- Assets/Scripts/PlayerNameInputField.cs
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -1,59 +1,41 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 using System.Collections;
 
-namespace TeamBronze.Hexwars {
-	/// <summary>
-	/// Player name input field. Let the user input his name, will appear above
-	/// the player in the game.
-	/// </summary>
-	[RequireComponent(typeof(InputField))]
-	public class PlayerNameInputField : MonoBehaviour {
-		#region Private Variables
 
-		// Store the PlayerPref Key to avoid typos
-		static string playerNamePrefKey = "PlayerName";
+namespace TeamBronze.HexWars
+{
+    /* Player name input field. Let the user input his name, will appear above the player in the game. */
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        /* Store the PlayerPref Key to avoid typos */
+        static string playerNamePrefKey = "PlayerName";
 
-		#endregion
+        /* MonoBehaviour method called on GameObject by Unity during initialization phase. */
+        void Start()
+        {
+            string defaultName = "";
+            InputField _inputField = this.GetComponent<InputField>();
+            if (_inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+            PhotonNetwork.playerName = defaultName;
+        }
 
-		#region MonoBehaviour CallBacks
+        /* Sets the name of the player, and save it in the PlayerPrefs for future sessions. */
+        public void SetPlayerName(string value)
+        {
+            PhotonNetwork.playerName = value + " "; /* force a trailing space string in case value is an empty string, else playerName would not be updated.*/
 
-		/// <summary>
-		/// MonoBehaviour method called on GameObject by Unity during initialization
-		/// phase.
-		/// </summary>
-		void Start() {
-
-			string defaultName = "";
-			InputField _inputField = this.GetComponent<InputField>();
-			if (_inputField!=null) {
-				if (PlayerPrefs.HasKey(playerNamePrefKey)) {
-					defaultName = PlayerPrefs.GetString(playerNamePrefKey);
-					_inputField.text = defaultName;
-				}
-			}
-
-			PhotonNetwork.playerName = defaultName;
-		}
-
-		#endregion
-
-		#region Public Methods
-
-		/// <summary>
-		/// Sets the name of the player and save it in the PlayerPrefs for
-		/// future sessions.
-		/// </summary>
-		/// <param name="value">The name of the Player </param>
-		public void SetPlayerName(string value) {
-			// #Important force a trailing space string in case value is an empty
-			// string, else playerName would not be updated.
-			PhotonNetwork.playerName = value + " ";
-
-			PlayerPrefs.SetString(playerNamePrefKey, value);
-		}
-
-		#endregion
-	}
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+    }
 }
