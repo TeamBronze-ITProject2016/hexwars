93f763ea221b279012d3742fe088a34da2a60986
Chris De Pasquale <chrisdippa@gmail.com>
2016-09-18 17:30:43 +1000

    Started collectable point script

--- Assets/Scripts/PointsCollectable.cs
+++ Assets/Scripts/PointsCollectable.cs
@@ -10,17 +10,44 @@ using System.Collections;
     -Should follow the player within some radius.
     -Once collected, should add points to the player who collected it and release 
     particles.
-    */
+*/
 
 public class PointsCollectable : MonoBehaviour {
 
-	// Use this for initialization
+    ParticleSystem particleSystem;
+
+    private float points = 0.0f;
+
+    /*The lifetime of this object*/
+    private float lifetime = 10.0f;
+    private float timeCreated = -1.0f;
+
+    private int particleCount = 0;
+
+	/*Initialise*/
 	void Start () {
-	
+        particleSystem = GetComponent<ParticleSystem>();
+        Debug.Assert(particleSystem);
+
+        particleCount = particleSystem.particleCount;
 	}
 	
-	// Update is called once per frame
+	/*Called once per frame.*/
 	void Update () {
-	
+	    if(timeCreated == -1.0f){
+            timeCreated = Time.time;
+        }
+
+        float lifetimePercent = (Time.time - timeCreated)/lifetime;
+
+        if (lifetimePercent > 1.0f)
+            Destroy(this);
 	}
+
+    /*Destroys this object and return the number of points it is worth*/
+    public float collect()
+    {
+        Destroy(this);
+        return points;
+    }
 }
