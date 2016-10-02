c83b93041f0cd8a589c258da08a48d6dbf019199
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-02 18:11:15 +1100

    added 'DestructibleObject' tag, DestructibleObjectSpawner to room scene, DestructibleObjectSpawner.cs and DestructibleObjectCollisionHandler.cs to Scripts folder, DestructibleObject prefab and tempdestructibleobjectsprite.png to Resources folder

---
+++ Assets/Scripts/DestructibleObjectCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TeamBronze.HexWars
+{
+    public class DestructibleObjectCollisionHandler : MonoBehaviour
+    {
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            /* TODO: Add points to player */
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+}
