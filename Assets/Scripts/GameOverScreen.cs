a64842afe8e4903a3f0b81a27fd2c320bb0b4631
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-18 01:04:28 +1100

    started game over screen

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public GameObject yourScoreTextObj;
+    public GameObject[] highScoresTextObjs;
+
+    public void BackToMenu()
+    {
+        Application.LoadLevel(0);
+    }
+}
---
+++ Assets/Scripts/ShowGameOverScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowGameOverScore : MonoBehaviour
+{
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
