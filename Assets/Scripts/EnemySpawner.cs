d34a28f91ba8dec087c2431715fdadd42f39441c
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-23 18:47:05 +1100

    EnemySpawner.cs cleanup

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿/* EnemySpawner.cs
+ * Authors: Nihal Mirpuri, William Pan, Jamie Grooby, Michael De Pasquale
+ * Description: Randomly spawns enemies up to a max amount
+ */
+
+using UnityEngine;
 using System.Collections;
 
 namespace TeamBronze.HexWars
@@ -29,6 +34,7 @@ namespace TeamBronze.HexWars
         [Tooltip("The radius around the enemy that must be clear in order to spawn")]
         public float spawnClearRadius = 5.0f;
 
+        // Enemy spawn timer
         private float t;
 
         void Start()
@@ -38,6 +44,10 @@ namespace TeamBronze.HexWars
 
         void Update()
         {
+            // Only master client spawns enemies
+            if (!PhotonNetwork.isMasterClient)
+                return;
+
             if (t > 0)
             {
                 t -= Time.deltaTime;
@@ -46,24 +56,31 @@ namespace TeamBronze.HexWars
             else
                 t = interval;
 
-            if (PhotonNetwork.isMasterClient && GameObject.FindGameObjectsWithTag("Enemy").Length < max)
+            // If current number of enemies is less than max, spawn a new enemy
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length < max)
             {
                 Vector3 spawnPos = new Vector3(Random.Range(x1, x2), Random.Range(y1, y2), 0.0f);
 
+                // Only spawn if the position is clear. Otherwise we will try again next interval
                 if (IsPosClear(spawnPos))
                     PhotonNetwork.InstantiateSceneObject(enemyPrefab.name, spawnPos, Quaternion.identity, 0, null);
             }
         }
 
-        bool IsPosClear(Vector3 position)
+        // Check if a position is clear for enemy spawning
+        private bool IsPosClear(Vector3 position)
         {
+            // Create a temporary object with a circle collider to check an area
             GameObject tempObj = new GameObject("Temp Obj");
             tempObj.transform.position = position;
             CircleCollider2D collider = tempObj.AddComponent<CircleCollider2D>();
             collider.isTrigger = true;
             collider.radius = spawnClearRadius;
+
+            // Cast collider at the position
             RaycastHit2D[] results = new RaycastHit2D[1];
             bool isPosClear = (collider.Cast(Vector2.zero, results) == 0);
+
             Destroy(tempObj);
 
             return isPosClear;
