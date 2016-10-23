347b8d34e4606424b1e2d978d7b04e3c8215d78d
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-23 19:24:01 +1100

    NicknameRenderer.cs cleanup

--- Assets/Scripts/NicknameRenderer.cs
+++ Assets/Scripts/NicknameRenderer.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿/* NicknameRenderer.cs
+ * Authors: Nihal Mirpuri, William Pan, Jamie Grooby, Michael De Pasquale
+ * Description: Renders a nickname over the gameobject based on its PhotonView owner
+ */
+
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
@@ -11,24 +16,31 @@ namespace TeamBronze.HexWars
 
         void Start()
         {
+            // Find the canvas object in the scene
             canvas = GameObject.FindGameObjectWithTag("Canvas");
 
+            // Create a new object to display the nickname
             textObj = new GameObject("NicknameText");
             textObj.transform.SetParent(canvas.transform);
 
+            // Add text
             Text textComponent = textObj.AddComponent<Text>();
             textComponent.text = photonView.owner.name;
+
+            // Style text
             textComponent.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
             textComponent.fontSize = 14;
             textComponent.fontStyle = FontStyle.Bold;
             textComponent.color = Color.white;
             textComponent.alignment = TextAnchor.MiddleCenter;
 
+            // Add outline
             Outline outlineComponent = textObj.AddComponent<Outline>();
             outlineComponent.effectDistance *= 0.75f;
             outlineComponent.effectColor = Color.black;
             outlineComponent.useGraphicAlpha = false;
 
+            // Double up the outline (looks better)
             Outline outlineComponent2 = textObj.AddComponent<Outline>();
             outlineComponent2.effectDistance *= 0.75f;
             outlineComponent2.effectColor = Color.black;
@@ -38,10 +50,11 @@ namespace TeamBronze.HexWars
         void LateUpdate()
         {
             RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-
             Vector2 viewportPos = Camera.main.WorldToViewportPoint(transform.position);
 
-            textObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(
+            // Update on-screen position of nickname
+            textObj.GetComponent<RectTransform>().anchoredPosition = new Vector2
+            (
                 ((viewportPos.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f)),
                 ((viewportPos.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f))
             );
@@ -49,6 +62,7 @@ namespace TeamBronze.HexWars
 
         void OnDestroy()
         {
+            // Destroy nickname when this game object is destroyed
             Destroy(textObj);
         }
     }
