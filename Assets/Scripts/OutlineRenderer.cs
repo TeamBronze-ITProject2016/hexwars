bca7e0f9d2659940e7d83101b323557cc4db023f
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-23 19:38:43 +1100

    OutlineRenderer.cs and PartParenting.cs cleanup

--- Assets/Scripts/OutlineRenderer.cs
+++ Assets/Scripts/OutlineRenderer.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿/* OutlineRenderer.cs
+ * Authors: Nihal Mirpuri, William Pan, Jamie Grooby, Michael De Pasquale
+ * Description: Renders outlines based on the path of this object's PolygonCollider2D
+ */
+
+using UnityEngine;
 using System.Collections;
 
 namespace TeamBronze.HexWars
 {
     public class OutlineRenderer : MonoBehaviour
     {
+        [Tooltip("Base width of the outline")]
         public float outlineWidth = 0.02f;
 
         private LineRenderer outline;
@@ -12,12 +18,15 @@ namespace TeamBronze.HexWars
 
         void Start()
         {
+            // Create new obj for the outline
             GameObject outlineObj = new GameObject("Outline");
             outlineObj.transform.SetParent(transform);
             outlineObj.transform.localPosition = new Vector3(0, 0, -1);
 
+            // Get path for the outline
             Vector2[] colliderPath = GetComponent<PolygonCollider2D>().GetPath(0);
 
+            // Add LineRenderer
             outline = outlineObj.AddComponent<LineRenderer>();
             outline.SetVertexCount(colliderPath.Length + 1);
             outline.useWorldSpace = false;
@@ -27,18 +36,22 @@ namespace TeamBronze.HexWars
 
             outlinePath = new Vector3[colliderPath.Length + 1];
 
+            // Set outline path to the path from PolygonCollider2D
             for (int i = 0; i < colliderPath.Length; i++)
             {
                 outlinePath[i] = transform.rotation * new Vector3(colliderPath[i].x, colliderPath[i].y, 0.0f);
             }
 
+            // Add final connection from end point to start point
             outlinePath[colliderPath.Length] = transform.rotation * new Vector3(colliderPath[0].x, colliderPath[0].y, 0.0f);
 
+            // Set the LineRenderer's path
             outline.SetPositions(outlinePath);
         }
 
         void Update()
         {
+            // Adjust outline width so that it appears to be the same width regardless of camera zoom
             float adjust = Camera.main.orthographicSize;
             outline.SetWidth(outlineWidth * adjust, outlineWidth * adjust);
         }
--- Assets/Scripts/PartParenting.cs
+++ Assets/Scripts/PartParenting.cs
@@ -1,16 +1,25 @@
-﻿using UnityEngine;
+﻿/* PartParenting.cs
+ * Authors: Nihal Mirpuri, William Pan, Jamie Grooby, Michael De Pasquale
+ * Description: Sets part parents to the appropriate player, so that their positions are synchronized properly.
+ */
+
+using UnityEngine;
 using System.Collections;
 
 namespace TeamBronze.HexWars
 {
     public class PartParenting : Photon.PunBehaviour
     {
+        // Called on instantiation by PUN
         public override void OnPhotonInstantiate(PhotonMessageInfo info)
         {
+            // Get ID of who instantiated the obj
             int ownerID = info.sender.ID;
 
+            // Get all non-local players
             GameObject[] playerObjs = GameObject.FindGameObjectsWithTag("Player");
 
+            // Find player with the correct ID and set as parent
             for(int i = 0; i < playerObjs.Length; i++)
             {
                 if(playerObjs[i].GetPhotonView().owner.ID == ownerID)
