087a1f58cc7002b75e4fd223bb4ba8fddf8c1483
jgrooby <jgrooby@student.unimelb.edu.au>
2016-10-23 16:55:40 +1100

    CameraFollow.cs code cleanup

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,22 +1,29 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/* CameraFollow.cs
+ * Authors: Nihal Mirpuri, William Pan, Jamie Grooby, Michael De Pasquale
+ * Description: Makes the camera appropriately follow the local player.
+ */
 
+using UnityEngine;
+using System.Collections;
 
 namespace TeamBronze.HexWars
 {
     public class CameraFollow : Photon.PunBehaviour
     {
+        [Tooltip("Base level of zoom for the camera")]
         public float zoomBase = 5;
+        
+        [Tooltip("Ratio to zoom out by when the player gets larger")]
         public float zoomRatio = 1;
 
-        Camera mainCamera;
+        // Reference to the main camera
+        private Camera mainCamera;
 
-        // Use this for initialization
         void Start()
         {
             Debug.Log("CameraFollow: Initialising....");
 
-            /* TODO: Check if attached to local player */
+            // Make sure that this script is only active for the local player
             if(photonView.isMine) {
                 mainCamera = Camera.main;
             }
@@ -24,29 +31,22 @@ namespace TeamBronze.HexWars
 
         void LateUpdate()
         {
-            setPos(mainCamera);
-            setZoom(mainCamera);
+            // Return if no reference to main camera
+            if (!mainCamera) { return; }
+
+            SetPos(mainCamera);
+            SetZoom(mainCamera);
         }
 
-        void setPos(Camera camera)
+        // Sets the position of the camera to the position of the player
+        private void SetPos(Camera camera)
         {
-            if (!mainCamera){
-                //Debug.Log("CameraFollow: mainCamera does not exist!");
-                return;
-            }
-
-            mainCamera.transform.position = new Vector3(
-                transform.position.x, transform.position.y, mainCamera.transform.position.z
-            );
+            mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, mainCamera.transform.position.z);
         }
 
-        void setZoom(Camera camera)
+        // Sets the zoom of the camera based on the current mass of the player and the given public variables
+        private void SetZoom(Camera camera)
         {
-            if (!mainCamera) {
-                //Debug.Log("CameraFollow: mainCamera does not exist!");
-                return;
-            }
-
             float mass = GetComponent<Rigidbody2D>().mass;
             mainCamera.orthographicSize = zoomBase + zoomRatio * mass;
         }
